def82994d42205190a31ee00e791ac3465c06db5
fathi <fathifidha2806@gmail.com>
2023-07-25 15:14:28 +0530

    commit

--- lib/news_hour_ui.dart
+++ lib/news_hour_ui.dart
@@ -4,13 +4,14 @@ import 'package:flutter/material.dart';
 
 import 'newshour_categories.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: news_hour(),
     debugShowCheckedModeBanner: false,
   ));
 }
-class news_hour extends StatefulWidget{
+
+class news_hour extends StatefulWidget {
   @override
   State<news_hour> createState() => _news_hourState();
 }
@@ -22,68 +23,75 @@ class _news_hourState extends State<news_hour> {
     'Item1',
     'Item2',
     'Item3',
-    'Item4',
-    'Item5',
-    'Item6',
-    'Item7',
-    'Item8',
   ];
   String? selectedValue;
 
   @override
-
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 3,
+    return DefaultTabController(
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
-            title: Row(
-              children: [
-                Text('News',style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold),),
-                Text('Hour',style: TextStyle(color: Colors.amber,fontSize: 16),),
-                Text(' - Admin',style: TextStyle(color: Colors.black,fontSize: 16),),
-                Padding(
-                  padding: const EdgeInsets.only(left: 310),
-                  child: Icon(Icons.settings_outlined,color: Colors.black38,size: 20,),
+          title: Row(
+            children: [
+              Text(
+                'News',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Hour',
+                style: TextStyle(color: Colors.amber, fontSize: 16),
+              ),
+              Text(
+                ' - Admin',
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 310),
+                child: Icon(
+                  Icons.settings_outlined,
+                  color: Colors.black38,
+                  size: 20,
                 ),
-              ],
-            ),
-
+              ),
+            ],
+          ),
           bottom: TabBar(
             indicatorColor: Colors.purpleAccent,
             unselectedLabelColor: Colors.black,
             labelColor: Colors.purpleAccent,
             tabs: [
-
-            Padding(
-              padding: const EdgeInsets.only(bottom: 13),
-              child: Text('Data Stat',style: TextStyle(fontSize: 16)),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.only(bottom: 13),
-              child: Text('Upload Item',style: TextStyle(fontSize: 16)),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 13),
-              child: Text('All Data',style: TextStyle(fontSize: 16)),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(bottom: 13),
+                child: Text('Data Stat', style: TextStyle(fontSize: 16)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 13),
+                child: Text('Upload Item', style: TextStyle(fontSize: 16)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 13),
+                child: Text('All Data', style: TextStyle(fontSize: 16)),
+              ),
+            ],
           ),
-          ),
-        body:Form(
+        ),
+        body: Form(
           key: validkey,
           child: Column(
             children: [
               Container(
-                margin: EdgeInsets.only(top: 22,left: 30,right: 30),
+                margin: EdgeInsets.only(top: 22, left: 30, right: 30),
                 height: 55,
                 width: 600,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                      color: Colors.grey[100]
-                ),
-                child:  DropdownButtonHideUnderline(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.grey[100]),
+                child: DropdownButtonHideUnderline(
                   child: DropdownButton2<String>(
                     isExpanded: true,
                     hint: const Row(
@@ -102,16 +110,16 @@ class _news_hourState extends State<news_hour> {
                     ),
                     items: items
                         .map((String item) => DropdownMenuItem<String>(
-                      value: item,
-                      child: Text(
-                        item,
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ))
+                              value: item,
+                              child: Text(
+                                item,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ))
                         .toList(),
                     value: selectedValue,
                     onChanged: (String? value) {
@@ -124,9 +132,8 @@ class _news_hourState extends State<news_hour> {
                       width: 160,
                       padding: const EdgeInsets.only(left: 14, right: 14),
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.grey[200]
-                      ),
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.grey[200]),
                       elevation: 2,
                     ),
                     iconStyleData: const IconStyleData(
@@ -156,69 +163,63 @@ class _news_hourState extends State<news_hour> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only( left: 20,right: 20,top: 10),
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
                 child: Container(
                   margin: EdgeInsets.all(10),
                   height: 54,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: Colors.black )
-                  ),
+                      border: Border.all(color: Colors.black)),
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 20,top: 6),
+                    padding: const EdgeInsets.only(left: 20, top: 6),
                     child: TextFormField(
                       decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Cover Picture Url',
-                        suffixIcon: Padding(
-                          padding: const EdgeInsets.only(right: 22),
-                          child: Icon(Icons.close),
-                        )
-                      ),
+                          border: InputBorder.none,
+                          hintText: 'Cover Picture Url',
+                          suffixIcon: Padding(
+                            padding: const EdgeInsets.only(right: 22),
+                            child: Icon(Icons.close),
+                          )),
                     ),
                   ),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only( left: 20,right: 20),
+                padding: const EdgeInsets.only(left: 20, right: 20),
                 child: Container(
                   margin: EdgeInsets.all(10),
                   height: 54,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: Colors.black )
-                  ),
+                      border: Border.all(color: Colors.black)),
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 20,top: 3),
+                    padding: const EdgeInsets.only(left: 20, top: 3),
                     child: TextField(
                       decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Title',
-                        suffixIcon: Padding(
-                          padding: const EdgeInsets.only(right: 22),
-                          child: Icon(Icons.close),
-                        )
-                      ),
+                          border: InputBorder.none,
+                          hintText: 'Title',
+                          suffixIcon: Padding(
+                            padding: const EdgeInsets.only(right: 22),
+                            child: Icon(Icons.close),
+                          )),
                     ),
                   ),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only( left: 20,right: 20),
+                padding: const EdgeInsets.only(left: 20, right: 20),
                 child: Container(
                   margin: EdgeInsets.all(10),
                   height: 96,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: Colors.black )
-                  ),
+                      border: Border.all(color: Colors.black)),
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 20,top: 22),
+                    padding: const EdgeInsets.only(left: 20, top: 22),
                     child: TextFormField(
                       decoration: InputDecoration(
                         border: InputBorder.none,
                         hintText: 'Description',
-
                       ),
                     ),
                   ),
@@ -227,14 +228,15 @@ class _news_hourState extends State<news_hour> {
               Row(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(left: 25,top: 50),
+                    padding: const EdgeInsets.only(left: 25, top: 50),
                     child: Checkbox(
                       checkColor: Colors.white,
                       value: isChecked,
                       onChanged: (bool? value) {
-                        setState(() {
-                          isChecked = value!;
-                        },
+                        setState(
+                          () {
+                            isChecked = value!;
+                          },
                         );
                       },
                     ),
@@ -243,12 +245,15 @@ class _news_hourState extends State<news_hour> {
                     padding: const EdgeInsets.only(top: 51),
                     child: Text('Notify to All Users'),
                   ),
-
                   Padding(
-                    padding: const EdgeInsets.only(left: 200,top: 50),
+                    padding: const EdgeInsets.only(left: 200, top: 50),
                     child: IconButton(
-                        onPressed: (){
-                      }, icon: Icon(Icons.remove_red_eye,color: Colors.blue,),),
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.remove_red_eye,
+                        color: Colors.blue,
+                      ),
+                    ),
                   ),
                   Padding(
                     padding: const EdgeInsets.only(top: 51),
@@ -257,40 +262,44 @@ class _news_hourState extends State<news_hour> {
                 ],
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 30,right: 30,top: 10),
+                padding: const EdgeInsets.only(left: 30, right: 30, top: 10),
                 child: Container(
                   height: 40,
                   color: Colors.amber,
-                  child: TextButton(onPressed: () {
-                    final valid = validkey.currentState!.validate();
-                    if(valid){
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => categories()));
-                    }else{
-                      return null;
-                    }
-                  },
+                  child: TextButton(
+                    onPressed: () {
+                      final valid = validkey.currentState!.validate();
+                      if (valid) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => categories()));
+                      } else {
+                        return null;
+                      }
+                    },
                     child: Padding(
                       padding: const EdgeInsets.only(left: 150),
                       child: Row(
-                      children: [
-                        Icon(Icons.save,color: Colors.white,),
-                        Text('   Save Data',style: TextStyle(color: Colors.white),)
-                      ],
-                  ),
+                        children: [
+                          Icon(
+                            Icons.save,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            '   Save Data',
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
                     ),
-
-
-
-                    ),
                   ),
+                ),
               ),
-
             ],
           ),
         ),
-
       ),
     );
-
   }
 }
